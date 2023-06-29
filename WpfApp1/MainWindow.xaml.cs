df7c1b0f087102b3442b709e49adfbc98e59906e
sergeymoshtakov <113889721+sergeymoshtakov@users.noreply.github.com>
2023-06-29 22:28:26 +0200

    Add files via upload

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public static class ColorCommands
+    {
+        public static readonly RoutedUICommand RedCommand = new RoutedUICommand("Red Command", "RedCommand", typeof(ColorCommands));
+        public static readonly RoutedUICommand GreenCommand = new RoutedUICommand("Green Command", "GreenCommand", typeof(ColorCommands));
+        public static readonly RoutedUICommand BlueCommand = new RoutedUICommand("Blue Command", "BlueCommand", typeof(ColorCommands));
+
+        static ColorCommands()
+        {
+            CommandManager.RegisterClassCommandBinding(typeof(UIElement), new CommandBinding(RedCommand, RedCommand_Executed));
+            CommandManager.RegisterClassCommandBinding(typeof(UIElement), new CommandBinding(GreenCommand, GreenCommand_Executed));
+            CommandManager.RegisterClassCommandBinding(typeof(UIElement), new CommandBinding(BlueCommand, BlueCommand_Executed));
+        }
+
+        private static void RedCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            var control = Keyboard.FocusedElement as Control;
+            if (control != null)
+                control.Background = Brushes.Red;
+        }
+
+        private static void GreenCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            var control = Keyboard.FocusedElement as Control;
+            if (control != null)
+                control.Background = Brushes.Green;
+        }
+
+        private static void BlueCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            var control = Keyboard.FocusedElement as Control;
+            if (control != null)
+                control.Background = Brushes.Blue;
+        }
+    }
+}
